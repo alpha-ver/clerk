98a98a15aa9fb6442704b6c45533a3cf448ddcd2
Alexey Vildyaev <hav0k@me.com>
2014-06-26 11:07:57 +0400

    modified:   config/routes.rb

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -2,4 +2,5 @@
 #= require jquery_ujs
 #= require turbolinks
 #= require bootstrap/tooltip
+
 #= require page
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get  'dashboard/category/:category_id'              => 'dashboard#document'
   get  'dashboard/generate/:template_id/:document_id' => 'dashboard#generate'
 
+
+  post 'dashboard/template' => 'dashboard#template'
+
   resources  :templates
   devise_for :users
 
